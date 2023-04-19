a281d1f13ce4fb3c5f2a61d60142ac1e4db34dcb
denispolovyan <denispolovyan@gmail.com>
2023-04-19 13:47:51 +0300

    fix: add vercel.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+	base: '/nuxt/'
+ },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
